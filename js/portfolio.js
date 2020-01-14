dc4a6ffa75314ac3aa137c001f37e7e50d568ac7
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-01-14 15:10:59 -0800

    add JS to portfolio
    Add slideshow to index.html

---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+  // open and close contact form
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    this.Close();
+  }
